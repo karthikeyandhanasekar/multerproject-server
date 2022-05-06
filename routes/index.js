6f51d10abb4c3c9caee6900ee40caa54d7609fb2
karthikeyan <dkkarthik2000@gmail.com>
2022-05-06 17:15:49 +0530

    works

--- routes/index.js
+++ routes/index.js
@@ -41,4 +41,31 @@ router.post("/upload", upload.single('file'), async (req, res, next) => {
   // console.log(req.file);
 })
 
+router.get("/download/:filename", async (req, res, next) => {
+
+
+  try {
+    const files = [...await gfs.find({ filename: req.params.filename }).toArray()]
+    console.log(files);
+
+
+    res.set({
+      "Accept-Ranges": "bytes",
+      "Content-Disposition": `attachment; filename=${files[0].filename}`,
+      "Content-Type": `${files[0].contentType}`
+    });
+
+    const readstream = gfs.openDownloadStreamByName(files[0].filename);
+    readstream.pipe(res);
+    // files?.map(file => {
+    //   var readstream = gfs.openDownloadStream(file._id)
+    //   res.set('Content-Type', file.mimetype);
+    //   return readstream.pipe(res);
+    // })
+  } catch (error) {
+    console.error(error.message);
+  }
+
+})
+
 module.exports = router;
