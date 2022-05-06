578718a7d62bdf66f951e2b90d33fe4f268f9370
karthikeyan <dkkarthik2000@gmail.com>
2022-05-06 15:42:43 +0530

    single file upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+require("dotenv").config()
+
+const { GridFsStorage } = require('multer-gridfs-storage');
+const { mongo, connection, } = require('mongoose');
+const multer = require('multer');
+
+let gfs
+
+//GFSBUCKET
+connection.once("open", () => {
+  gfs = new mongo.GridFSBucket(connection.db, {
+    bucketName: 'GFSbucket' // a bucket collection name
+  });
+})
+const storage = new GridFsStorage({
+  url: process.env.MONGODB_URL,
+  file: (req, file) => {
+
+    return new Promise((resolve, reject) => {
+      const fileInfo = {
+        filename: file.originalname,
+        bucketName: 'GFSbucket' // a collection name
+      };
+      resolve(fileInfo);
+    });
+  }
+});
+
+const upload = multer({ storage: storage })
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post("/upload", upload.single('file'), async (req, res, next) => {
+
+  // console.log(req.file);
+})
 
 module.exports = router;
